717071eb11fb22bba34769e24b9c97336107fcbc
chihwan <clghks@quicket.co.kr>
2015-11-27 18:18:08 +0900

    Data Binding (Observable) - 사용전

---
+++ DataBinding_Observable_Old/app/src/main/java/com/clghks/databinding_observable_old/MainActivity.java
@@ -0,0 +1,45 @@
+package com.clghks.databinding_observable_old;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView firstName;
+    private TextView lastName;
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user = new User("Tes1t", "User");
+        initView();
+        setUserData();
+    }
+
+    private void initView() {
+        firstName = (TextView)findViewById(R.id.tv_firstName);
+        lastName = (TextView)findViewById(R.id.tv_lastName);
+        firstNameEditText = (EditText)findViewById(R.id.firstName);
+        lastNameEditText = (EditText)findViewById(R.id.lastName);
+    }
+
+    private void setUserData() {
+        firstName.setText(user.getFirstName());
+        lastName.setText(user.getLastName());
+    }
+
+    public void editButtonClick(View view){
+        user.setFirstName(firstNameEditText.getText().toString());
+        user.setLastName(lastNameEditText.getText().toString());
+
+        setUserData();
+    }
+}
